9fb83af6ad13ddc941987cc517efe0d8fb55ca52
高江华 <g598670138@163.com>
2023-08-04 18:03:37 +0800

    fix: 🧩 修复store

--- src/middleware/auth.global.ts
+++ src/middleware/auth.global.ts
@@ -2,7 +2,7 @@ import { useUserStore } from '~/store';
 export default defineNuxtRouteMiddleware(async (to) => {
     const userStore = useUserStore();
     // 获取用户信息
-    let { id } = userStore.$state.state.userInfo;
+    let { id } = userStore.state.userInfo;
     if (!id) {
         // 用户不存在之后的操作
     }
